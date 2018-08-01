e8a25d293154ff21c246e49856e9a322d77b7781
AllaSusmitha <41768199+AllaSusmitha@users.noreply.github.com>
2018-08-01 15:07:07 +0530

    Create susmi12.py

---
+++ susmi12.py
@@ -0,0 +1,10 @@
+x=float(input())
+y=float(input())
+z=float(input())
+if (x >= y) and (x >= z):
+	largest =x
+elif (y >= x) and (y >= z):
+	largest = y
+else:
+	largest = z
+print("largest of three numbers ", largest)
